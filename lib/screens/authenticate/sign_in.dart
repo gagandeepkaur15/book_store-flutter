c7eb5c1a5c1077ff320a3208ce1144ba67191b4e
Gagandeep <gagandeepkaur1523@gmail.com>
2022-12-07 17:26:17 +0530

    Added sign in anonymously button

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:book_store/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -8,14 +9,29 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService(); //instance of the AuthService class
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20,horizontal: 50),
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
         child: Column(
           children: [
-            ElevatedButton(onPressed: (){}, child: Text('Sign in anonymously'))
+            ElevatedButton(
+              onPressed: () async {
+                dynamic result = await _auth
+                    .signInAnon(); //dynamic as it can be the user or null
+                if (result == null) {
+                  print('error signing in');
+                } else {
+                  print('signed in');
+                  print(result);
+                  print(result.uid);
+                }
+              },
+              child: Text('Sign in anonymously'),
+            )
           ],
         ),
       ),
