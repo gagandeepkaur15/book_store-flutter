57b82a3614e449b031fe29ab614a7cc2bf6f7c5c
Gagandeep <gagandeepkaur1523@gmail.com>
2022-12-07 21:15:47 +0530

    Added the listener

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,12 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import './authenticate/authenticate.dart';
 import './home/home.dart';
-import 'package:flutter/material.dart';
+import '../models/user.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Authenticate();
+    final user = Provider.of<MyUser?>(context);
+    print(user);
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
