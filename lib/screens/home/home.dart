a006980a363acdb2a40d155e3a7eb7fe2467d9b4
Gagandeep <gagandeepkaur1523@gmail.com>
2022-12-08 16:34:00 +0530

    Added logout button

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:book_store/services/auth.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Home');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Florish and Blotts Book Store'),
+        elevation: 0,
+        actions: <Widget>[
+          TextButton.icon(
+            icon: const Icon(Icons.person),
+            label: const Text('logout'),
+            onPressed: (() async {
+              await _auth.signOutUser();
+            }),
+          ),
+        ],
+      ),
+    );
   }
 }
