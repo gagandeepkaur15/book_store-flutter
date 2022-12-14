2cb28044e4c282ae35fcfbb0a9ed304dcda46957
Gagandeep <gagandeepkaur1523@gmail.com>
2022-12-14 19:08:52 +0530

    Added CircularProgressIndicator

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -12,6 +12,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService(); //instance of the AuthService class
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   String email = '';
   String password = '';
@@ -19,123 +20,132 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Colors.brown,
-        title: const Text('Sign In'),
-        actions: <Widget>[
-          TextButton.icon(
-            icon: const Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-            label: const Text(
-              'Sign Up',
-              style: TextStyle(color: Colors.white),
-            ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const Register(),
-                ),
-              );
-            },
-          )
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                validator: (val) => val!.isEmpty ? 'Enter the email' : null,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                validator: (val) =>
-                    val!.length < 6 ? 'Enter atleast 6 characters' : null,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                ),
-                obscureText: true,
-                onChanged: (value) {
-                  password = value;
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.brown),
-                ),
-                onPressed: () async {
-                  if (_formKey.currentState!.validate()) {
-                    dynamic result = await _auth.signIn(email, password);
-                    if (result == null) {
-                      setState(() {
-                        error = 'Failed to Sign In';
-                      });
-                    }
-                  }
-                },
-                child: const Text(
-                  'Sign In',
-                  style: TextStyle(
+    return loading
+        ? const CircularProgressIndicator(color: Colors.brown,)
+        : Scaffold(
+            appBar: AppBar(
+              automaticallyImplyLeading: false,
+              backgroundColor: Colors.brown,
+              title: const Text('Sign In'),
+              actions: <Widget>[
+                TextButton.icon(
+                  icon: const Icon(
+                    Icons.person,
                     color: Colors.white,
                   ),
+                  label: const Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const Register(),
+                      ),
+                    );
+                  },
+                )
+              ],
+            ),
+            body: Container(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      validator: (val) =>
+                          val!.isEmpty ? 'Enter the email' : null,
+                      decoration: const InputDecoration(
+                        hintText: 'Email',
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      validator: (val) =>
+                          val!.length < 6 ? 'Enter atleast 6 characters' : null,
+                      decoration: const InputDecoration(
+                        hintText: 'Password',
+                      ),
+                      obscureText: true,
+                      onChanged: (value) {
+                        password = value;
+                      },
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.brown),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          dynamic result = await _auth.signIn(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'Failed to Sign In';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                      child: const Text(
+                        'Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      error,
+                      style: TextStyle(
+                        color: Colors.red,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.brown),
+                      ),
+                      onPressed: () async {
+                        dynamic result = await _auth
+                            .signInAnon(); //dynamic as it can be the user or null
+                        if (result == null) {
+                          print('error signing in');
+                        } else {
+                          print('signed in');
+                          print(result);
+                          print(result.uid);
+                        }
+                      },
+                      child: Text('Sign in anonymously'),
+                    )
+                  ],
                 ),
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.brown),
-                ),
-                onPressed: () async {
-                  dynamic result = await _auth
-                      .signInAnon(); //dynamic as it can be the user or null
-                  if (result == null) {
-                    print('error signing in');
-                  } else {
-                    print('signed in');
-                    print(result);
-                    print(result.uid);
-                  }
-                },
-                child: Text('Sign in anonymously'),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
