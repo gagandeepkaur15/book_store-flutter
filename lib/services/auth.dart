f7401ca20b5a4d04591c07c314bf12c44fbd3de3
Gagandeep <gagandeepkaur1523@gmail.com>
2022-12-14 16:28:25 +0530

    Added the functions to sign in and register

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -36,8 +36,30 @@ class AuthService {
   }
 
   //sign in with email and password
+  Future signIn(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   //register with email and password
+  Future register(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User? user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
 
   //sign out
   Future signOutUser() async {
